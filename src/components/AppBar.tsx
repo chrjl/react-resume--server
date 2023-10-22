f90bfc88bfc45ad2537b77d86e0a557533ba1d0a
chrjl <73675826+chrjl@users.noreply.github.com>
2023-10-21 23:00:46 -0700

    feat(AppBar,AppContext): update data source on submit
    - AppBar: adds submit and reset buttons to the data source form. On
      submit, use the passed-in state setter to update source, path, format.
    
    - AppContext: the state setter updates the global app context in the
      parent component

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,60 +1,71 @@
-import { useContext } from 'react';
-
-import { AppContext, AppContextType } from '../contexts/AppContext';
+import { AppContextType } from '../contexts/AppContext';
 
 interface AppBarProps {
   setAppContext: React.Dispatch<React.SetStateAction<AppContextType>>;
 }
 
 export default function AppBar({ setAppContext }: AppBarProps) {
-  const appContext = useContext(AppContext);
-
   return (
     <>
       <form
-        onSubmit={(e) => e.preventDefault()}
-      >
-        <h1>DATA SOURCE</h1>
-        <fieldset
-          id="format"
-          onChange={(e) => {
-            const { name, value } = e.target as HTMLInputElement;
-            if (name !== 'format') return;
+        onSubmit={(e) => {
+          e.preventDefault();
 
+          const form = e.target as HTMLFormElement;
+
+          try {
+            // update source
+            switch (form.source.value) {
+              case 'url':
+                setAppContext((prevAppContext) => ({
+                  ...prevAppContext,
+                  source: 'url',
+                  path: form.url.value,
+                }));
+                break;
+
+              case 'file':
+                setAppContext((prevAppContext) => ({
+                  ...prevAppContext,
+                  source: 'file',
+                  path: form.file.files[0]?.name,
+                }));
+                break;
+
+              default:
+                break;
+            }
+
+            // update format
             setAppContext((prevAppContext) => ({
               ...prevAppContext,
-              format: value,
+              format: form.format.value,
             }));
-          }}
-        >
+          } catch (e) {
+            alert('error!');
+            throw e;
+          }
+        }}
+      >
+        <h1>DATA SOURCE</h1>
+        <fieldset id="format-container">
           <legend>Format:</legend>
           <label>
             <input type="radio" name="format" value="json" defaultChecked />
             JSON resume
           </label>
           <label>
-            <input type="radio" name="format" value="markdown" />
+            <input type="radio" name="format" value="markdown" disabled />
             Markdown
           </label>
         </fieldset>
 
-        <fieldset
-          id="source"
-          onChange={(e) => {
-            const { name, value } = e.target as HTMLInputElement;
-            if (name !== 'source') return;
-
-            setAppContext((prevAppContext) => ({
-              ...prevAppContext,
-              source: value,
-            }));
-          }}
-        >
+        <fieldset id="source-container">
           <legend>Source:</legend>
           <label>
             <input type="radio" name="source" value="url" defaultChecked />
             URL
-            <input type="text" />
+            <input type="text" id="url" />
           </label>
 
           <br />
@@ -62,9 +73,12 @@ export default function AppBar({ setAppContext }: AppBarProps) {
           <label>
             <input type="radio" name="source" value="file" />
             File
-            <input type="file" />
+            <input type="file" id="file" />
           </label>
         </fieldset>
+
+        <button type="submit">submit</button>
+        <button type="reset">reset</button>
       </form>
     </>
   );

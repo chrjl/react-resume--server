b4e6e23db3d27a87c6d5d7a247f11202296ab3d6
chrjl <73675826+chrjl@users.noreply.github.com>
2023-10-24 23:27:03 -0700

    feat(Document): import and render template
    - Load parsed resume from AppContext
    - Import template (array of components) from package
    - Render components using data from AppContext

--- src/components/Document.tsx
+++ src/components/Document.tsx
@@ -1,4 +1,18 @@
-export default function Document() {
-  return <>Document</>;
-}
+import { useContext } from 'react';
 
+import sections from 'template';
+import { AppContext } from '../contexts/AppContext';
+
+export default function Document() {
+  const appContext = useContext(AppContext);
+  const { resume } = appContext;
+
+  return sections.map(({ id, Component }) =>
+    // type guard data sent to template component props
+    id in resume && resume[id] ? (
+      <section key={id} id={id}>
+        <Component data={resume[id]} />
+      </section>
+    ) : null
+  );
+}

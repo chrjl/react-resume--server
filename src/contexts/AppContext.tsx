929619a2f70043174021ad7b930698667d9aa491
chrjl <73675826+chrjl@users.noreply.github.com>
2023-10-28 17:54:25 -0700

    refactor(types): import shared types from workspace package

--- src/contexts/AppContext.tsx
+++ src/contexts/AppContext.tsx
@@ -1,5 +1,5 @@
 import { createContext } from 'react';
-import { ResumeObject } from '@reactresume/jsonresume-parser';
+import { ResumeObject } from '@reactresume/types';
 
 export const AppContext = createContext<AppContextType>({
   source: 'url',

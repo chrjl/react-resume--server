d74b0a2c192f177672e55ac77afd3db30c8b937b
chrjl <73675826+chrjl@users.noreply.github.com>
2023-12-01 16:48:34 -0800

    build: install selfhosted fonts
    Inter Variable, Arimo Variable, Cousine

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,18 @@ import AppBar from './components/AppBar';
 import Document from './components/Document';
 import DebugArea from './components/DebugArea';
 
+import '@fontsource/arimo/latin-400.css';
+import '@fontsource/arimo/latin-400-italic.css';
+import '@fontsource/arimo/latin-700.css';
+import '@fontsource/arimo/latin-700-italic.css';
+import '@fontsource/inter/latin-400.css';
+import '@fontsource/inter/latin-600.css';
+import '@fontsource/inter/latin-700.css';
+import '@fontsource/cousine/latin-400.css';
+import '@fontsource/cousine/latin-400-italic.css';
+import '@fontsource/cousine/latin-700.css';
+import '@fontsource/cousine/latin-700-italic.css';
+
 import './styles/App.css';
 
 export default function App() {

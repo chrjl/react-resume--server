f9505e17184612de4c9b2ff9c270ba6ec8e1d5ec
chrjl <73675826+chrjl@users.noreply.github.com>
2024-05-21 21:50:33 -0700

    feat(root): dropdown NavLinks for parsed roots

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,4 +1,7 @@
 import { NavLink, Outlet } from 'react-router-dom';
+import 'bootstrap';
+
+import { useData } from '../contexts/DataContext';
 
 import githubMark from '../assets/github-mark-white.svg';
 
@@ -30,9 +33,64 @@ export default function Root() {
               <li className="nav-item">
                 <NavLink to="/raw" className="nav-link">
                   Raw
-                </NavLink>
+                </a>
+                <ul className="dropdown-menu">
+                  <li className="nav-item">
+                    <NavLink to="/raw" end className="dropdown-item">
+                      ALL
+                    </NavLink>
+                  </li>
+                  <hr />
+                  {Object.keys(raw).map((sectionId) => (
+                    <li key={sectionId} className="nav-item">
+                      <NavLink
+                        className="dropdown-item"
+                        to={`/raw/${sectionId}`}
+                      >
+                        {sectionId}
+                      </NavLink>
+                    </li>
+                  ))}
+                </ul>
               </li>
             )}
+
+            {parsed && (
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Parsed
+                </a>
+                <ul className="dropdown-menu">
+                  {Object.keys(parsed).map((sectionId) => (
+                    <li key={sectionId} className="nav-item">
+                      <NavLink
+                        className="dropdown-item"
+                        to={`/parsed/${sectionId}`}
+                      >
+                        {sectionId}
+                      </NavLink>
+                    </li>
+                  ))}
+                </ul>
+              </li>
+            )}
+
+            <li className="nav-item">
+              <NavLink to="/templates" className="nav-link disabled">
+                Templates
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/document" className="nav-link disabled">
+                Document
+              </NavLink>
+            </li>
           </ul>
 
           <ul className="navbar-nav">

e50c96a104364fc1c3955cc97f079d9238bad208
chrjl <73675826+chrjl@users.noreply.github.com>
2024-05-22 11:52:32 -0700

    feat(raw): add views by sectionId

--- src/routes/raw.tsx
+++ src/routes/raw.tsx
@@ -1,15 +1,28 @@
+import { useLoaderData } from 'react-router-dom';
+
 import { JsonView } from 'react-json-view-lite';
 import 'react-json-view-lite/dist/index.css';
 
 import { useData } from '../contexts/DataContext';
 
+export function loader({ params }) {
+  return params.sectionId;
+}
+
 export default function Raw() {
   const data = useData();
+  const sectionId = useLoaderData();
+
+  const view = data.raw
+    ? sectionId
+      ? { [sectionId]: data.raw[sectionId] }
+      : data.raw
+    : 'no data';
 
   return (
     <div className="container">
       <code>
-        <JsonView data={data.raw} clickToExpandNode={true} />
+        <JsonView data={view} clickToExpandNode={true} />
       </code>
     </div>
   );

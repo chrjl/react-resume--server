7b820e8f91555865812505422bc707eccb86f70b
chrjl <73675826+chrjl@users.noreply.github.com>
2024-05-22 09:20:10 -0700

    refactor(parsed): split ResumeEntry into own component

--- src/routes/parsed.tsx
+++ src/routes/parsed.tsx
@@ -1,4 +1,5 @@
 import { useLoaderData } from 'react-router-dom';
+import { ResumeEntry } from '../../packages/types/dist';
 
 import { useData } from '../contexts/DataContext';
 
@@ -10,63 +11,84 @@ export default function Parsed() {
   const sectionId = useLoaderData();
 
   const data = useData();
-  const sectionData =
+  const entries: ResumeEntry[] | null =
     data?.parsed && data.parsed[sectionId] ? data.parsed[sectionId] : null;
-
-  console.log(sectionData);
 
   return (
     <div className="container">
       <h1 style={{ textTransform: 'capitalize' }}>{sectionId}</h1>
       <ul className="list-group">
-        {sectionData
-          ? sectionData.map(
+        {entries
+          ? entries.map(
               ({ title, subtitle, note, description, highlights }, i) => (
                 <li key={i} className="list-group-item list-group-item-action">
-                <>
-                  <li>
-                    <dl>
-                      {title && (
-                        <>
-                          <dt>Title</dt>
-                          <dd>{title}</dd>
-                        </>
-                      )}
-
-                      {subtitle && (
-                        <>
-                          <dt>Subtitle</dt>
-                          <dd>{subtitle}</dd>
-                        </>
-                      )}
-
-                      {note && (
-                        <>
-                          <dt>Note</dt>
-                          <dd>{note}</dd>
-                        </>
-                      )}
-
-                      {description && (
-                        <>
-                          <dt>Description</dt>
-                          <dd>{description}</dd>
-                        </>
-                      )}
-
-                      {highlights && (
-                        <>
-                          <dt>Highlights</dt>
-                          <dd>{highlights}</dd>
-                        </>
-                      )}
-                    </dl>
-                  </li>
-                </>
+                  <ResumeEntryDetail
+                    title={title}
+                    subtitle={subtitle}
+                    note={note}
+                    description={description}
+                    highlights={highlights}
+                  />
+                </li>
               )
             )
           : 'no data'}
       </ul>
     </div>
+  );
+}
+
+interface ResumeEntryDetailProps {
+  title: React.ReactNode;
+  subtitle: React.ReactNode;
+  note: React.ReactNode;
+  description: React.ReactNode;
+  highlights: React.ReactNode;
+}
+
+function ResumeEntryDetail({
+  title,
+  subtitle,
+  note,
+  description,
+  highlights,
+}: ResumeEntryDetailProps) {
+  return (
+    <dl>
+      {title && (
+        <>
+          <dt>Title</dt>
+          <dd>{title}</dd>
+        </>
+      )}
+
+      {subtitle && (
+        <>
+          <dt>Subtitle</dt>
+          <dd>{subtitle}</dd>
+        </>
+      )}
+
+      {note && (
+        <>
+          <dt>Note</dt>
+          <dd>{note}</dd>
+        </>
+      )}
+
+      {description && (
+        <>
+          <dt>Description</dt>
+          <dd>{description}</dd>
+        </>
+      )}
+
+      {highlights && (
+        <>
+          <dt>Highlights</dt>
+          <dd>{highlights}</dd>
+        </>
+      )}
+    </dl>
   );
 }

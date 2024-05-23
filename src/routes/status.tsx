c9f9286ebd2e267438dc9591dcd50077079624b7
chrjl <73675826+chrjl@users.noreply.github.com>
2024-05-23 15:41:44 -0700

    feat(status): implement template to SectionStatus

--- src/routes/status.tsx
+++ src/routes/status.tsx
@@ -1,3 +1,5 @@
+import templates from '@reactresume/template';
+
 import { useMeta } from '../contexts/MetaContext';
 import { useData } from '../contexts/DataContext';
 
@@ -43,13 +45,18 @@ function SectionStatus() {
   const { raw, parsed } = useData();
 
   if (!raw) {
-    return <code>no data</code>
+    return <code>no data</code>;
   }
 
   const sectionList = new Set<string>();
 
-  Object.keys(parsed).forEach((section) => sectionList.add(section));
-  Object.keys(raw).forEach((section) => sectionList.add(section));
+  const rawSectionsList = Object.keys(raw);
+  const parsedSectionsList = Object.keys(parsed);
+  const templateSectionsList = templates.map((t) => t.id);
+
+  parsedSectionsList.forEach((section) => sectionList.add(section));
+  rawSectionsList.forEach((section) => sectionList.add(section));
+  templateSectionsList.forEach((section) => sectionList.add(section));
 
   return (
     <table className="table d-inline-block">
@@ -58,6 +65,7 @@ function SectionStatus() {
           <th>Section</th>
           <th>Raw</th>
           <th>Parsed</th>
+          <th>Templates</th>
         </tr>
       </thead>
       <tbody>
@@ -65,10 +73,13 @@ function SectionStatus() {
           <tr>
             <th>{s}</th>
             <td>
-              <code>{Object.keys(raw).includes(s).toString()}</code>
+              <code>{rawSectionsList.includes(s).toString()}</code>
             </td>
             <td>
-              <code>{Object.keys(parsed).includes(s).toString()}</code>
+              <code>{parsedSectionsList.includes(s).toString()}</code>
+            </td>
+            <td>
+              <code>{templateSectionsList.includes(s).toString()}</code>
             </td>
           </tr>
         ))}

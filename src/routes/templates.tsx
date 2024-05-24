2089f6c415353cbe5a73d3c19e503f2ceef7c465
chrjl <73675826+chrjl@users.noreply.github.com>
2024-05-23 21:41:51 -0700

    style(template): apply document style via CSS module
    - Global document style: .container class
    - Section-specific styles: .section class

--- src/routes/templates.tsx
+++ src/routes/templates.tsx
@@ -2,6 +2,8 @@ import { useLoaderData } from 'react-router-dom';
 import templates from '@reactresume/template';
 import { useData } from '../contexts/DataContext';
 
+import styles from '../styles/document.module.css';
+
 export function loader({ params }) {
   return params.sectionId;
 }
@@ -12,7 +14,13 @@ export default function Template() {
 
   const Template = templates.find((t) => t.id === sectionId)?.Component;
   return data ? (
-    <Template data={data} />
+    <div className={styles.container}>
+      <section>
+        <div className={styles[sectionId]}>
+          <Template data={data} />
+        </div>
+      </section>
+    </div>
   ) : (
     <code>no data</code>
   );

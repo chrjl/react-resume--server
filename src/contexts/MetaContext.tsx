c677008857e8a217480635ca74cb985f781ae72d
chrjl <73675826+chrjl@users.noreply.github.com>
2024-05-24 07:33:30 -0700

    feat(MetaContext): add section display status
    Read templateSectionList from templates import
    Initially set all sections hidden

--- src/contexts/MetaContext.tsx
+++ src/contexts/MetaContext.tsx
@@ -1,11 +1,20 @@
 import { createContext, useContext, useReducer } from 'react';
+import templates from '@reactresume/template';
+
+interface MetaStateSource {
+  type: 'remote' | 'file' | null;
+  url?: string;
+  fileName?: string;
+}
+
+interface MetaStateSection {
+  id: string;
+  visible: boolean;
+}
 
 interface MetaState {
-  source: {
-    type: 'remote' | 'file' | null;
-    url?: string;
-    fileName?: string;
-  };
+  source: MetaStateSource;
+  display: MetaStateSection[];
 }
 
 type Action = {
@@ -13,10 +22,13 @@ type Action = {
   source: { type: 'remote' | 'file'; url?: string; fileName?: string };
 };
 
+const templateSectionList = templates.map((s) => s.id);
+
 const initialState: MetaState = {
   source: {
     type: null,
   },
+  display: templateSectionList.map((s) => ({ id: s, visible: false })),
 };
 
 function metaReducer(metaState: MetaState, action: Action) {
